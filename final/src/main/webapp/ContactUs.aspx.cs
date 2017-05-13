52067b266dea16e560798c052eb6a21b7e321b60
Saeed Qaffaf <saeedq94@live.com>
2017-05-13 04:43:18 +0300

    Add files via upload

---
+++ final/src/main/webapp/ContactUs.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class bootstrap_shop_ContactUs : System.Web.UI.Page
+{
+    public Label Label1;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Label1.Text =
+            DateTime.Now.ToLocalTime().ToLongTimeString();
+    }
+}
